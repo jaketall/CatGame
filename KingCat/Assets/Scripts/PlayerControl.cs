3e311d9618f5fec97b01cb7143459828bb84eb70
Ryan <rfarina51@gmail.com>
2020-02-12 16:22:02 -0800

    added cats + player controller

--- KingCat/Assets/Scripts/PlayerControl.cs
+++ KingCat/Assets/Scripts/PlayerControl.cs
@@ -19,16 +19,21 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal_input = Input.GetAxis("Horizontal");
-        float vertical_input = Input.GetAxis("Vertical");
-        
-        rb.AddForce(thrust * horizontal_input * Vector3.right);
-        rb.AddForce(thrust * vertical_input * Vector3.forward);
+
+        //rb.AddForce(thrust * horizontal_input * Vector3.right);
+        //rb.AddForce(thrust * vertical_input * Vector3.forward);
         // transform.Translate(Time.deltaTime * horizontal_input * Vector3.right * speed);
         // transform.Translate(Time.deltaTime * vertical_input * Vector3.forward * speed);
-        
-    }
 
+
+    }
+    private void FixedUpdate()
+    {
+        float horizontal_input = Input.GetAxis("Horizontal");
+        float vertical_input = Input.GetAxis("Vertical");
+        Vector3 newPos = new Vector3(horizontal_input, 0, vertical_input) * thrust;
+        rb.MovePosition(transform.position + newPos);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("powerup"))
