80870e321553bbd5e1a45b9a650b905ae5d6f77a
Ryan <rfarina51@gmail.com>
2020-02-13 10:31:42 -0800

    animation, movement and dashing

--- KingCat/Assets/Scripts/PlayerControl.cs
+++ KingCat/Assets/Scripts/PlayerControl.cs
@@ -7,13 +7,22 @@ public class PlayerControl : MonoBehaviour
 {
     public float xClamp;
     public float yClamp;
+    public float dashForce;
     public float thrust; 
     private Rigidbody rb;
-    
+
+    private Animator catAnim;
+    private int runHash = Animator.StringToHash("Run");
+    private int dashHash = Animator.StringToHash("Dash");
+    private int wasHitHash = Animator.StringToHash("Was_Hit");
+
+    private bool isDashing;
+
     // Start is called before the first frame update
     void Start()
-    { 
-        rb = GetComponent<Rigidbody>();   
+    {
+        rb = GetComponent<Rigidbody>();
+        catAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,10 +38,38 @@ public class PlayerControl : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        catAnim.SetBool(dashHash, false);
         float horizontal_input = Input.GetAxis("Horizontal");
         float vertical_input = Input.GetAxis("Vertical");
         Vector3 newPos = new Vector3(horizontal_input, 0, vertical_input) * thrust;
-        rb.MovePosition(transform.position + newPos);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isDashing)
+            {
+                //dash
+                catAnim.SetBool(dashHash, true);
+                isDashing = true;
+                StartCoroutine(Dash());
+            }
+        }
+        else if (!newPos.Equals(Vector3.zero))
+        {
+            // run
+            if (!isDashing)
+            {
+                catAnim.SetBool(runHash, true);
+                rb.MovePosition(transform.position + newPos);
+                //transform.rotation = Quaternion.LookRotation(newPos);
+                rb.MoveRotation(Quaternion.LookRotation(newPos));
+            }
+        }
+        else
+        {
+            // not moving -> back to idle
+            catAnim.SetBool(runHash, false);
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -40,5 +77,28 @@ public class PlayerControl : MonoBehaviour
         {
             Destroy(other.gameObject);          
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("is dashing? " + isDashing);
+            if (isDashing)
+            {
+                collision.gameObject.GetComponent<Animator>().SetTrigger(
+                    wasHitHash);
+            }
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        yield return new WaitForSeconds(0.3f);
+        rb.AddRelativeForce(Vector3.forward * dashForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(0.3f);
+        rb.velocity = Vector3.zero;
+        isDashing = false;
+
     }
 }
